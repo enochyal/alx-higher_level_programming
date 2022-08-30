e31c0a5905c1fb794d323c1b27887db69874d590
enochyal <enochyal@gmail.com>
2022-08-30 10:26:18 +0300

    my fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for x in range(ord("a"), ord("z") + 1):
-    if chr(x) != 'q' and chr(x) != 'e':
-        print("{}".format(chr(x)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if i == ord('e') or i == ord('q'):
+        continue
+    print("{}".format(chr(i)), end="")`
