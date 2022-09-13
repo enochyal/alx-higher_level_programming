9f6e43f357085e2a1897fccef94570c23c7a841f
enochyal <enochyal@gmail.com>
2022-09-13 22:32:09 +0300

    first on class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Square is defined by shape
+    
+    Attributes:
+        size (int)/: size of square
+    """
+    def __init__(self, size):
+        """
+        Initialize method 
+
+        Args:
+            size(int): size of square
+        """
+        self.size = size
